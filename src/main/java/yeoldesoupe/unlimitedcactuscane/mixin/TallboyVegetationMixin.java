010dedcb262a84bc676a8044c3a1e5812eff9afb
Yoghurt4C <yoghurt4242@gmail.com>
2021-04-29 00:59:51 +0300

    8.9kb -> 3.87kb. massive gains!

--- src/main/java/yeoldesoupe/unlimitedcactuscane/mixin/TallboyVegetationMixin.java
+++ src/main/java/yeoldesoupe/unlimitedcactuscane/mixin/TallboyVegetationMixin.java
@@ -1,12 +1,11 @@
 package yeoldesoupe.unlimitedcactuscane.mixin;
 
-import net.minecraft.block.CactusBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Constant;
 
-@Mixin(CactusBlock.class)
-public class CactusBlockMixin {
+@Mixin(targets = {"net.minecraft.block.SugarCaneBlock", "net.minecraft.block.CactusBlock"})
+public abstract class TallboyVegetationMixin {
 	@ModifyConstant(method = "randomTick", constant = @Constant(intValue = 3))
 	private int maxint(int x) {
 		return Integer.MAX_VALUE;
