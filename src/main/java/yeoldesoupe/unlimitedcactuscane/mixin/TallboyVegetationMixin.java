25982053fb5f84ff5e54d7b168ae13029a21f7d0
Yoghurt4C <yoghurt4242@gmail.com>
2021-04-29 02:09:38 +0300

    slimey face

--- src/main/java/yeoldesoupe/unlimitedcactuscane/mixin/TallboyVegetationMixin.java
+++ src/main/java/yeoldesoupe/unlimitedcactuscane/mixin/TallboyVegetationMixin.java
@@ -1,10 +1,12 @@
 package yeoldesoupe.unlimitedcactuscane.mixin;
 
+import net.minecraft.block.CactusBlock;
+import net.minecraft.block.SugarCaneBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Constant;
 
-@Mixin(targets = {"net.minecraft.block.SugarCaneBlock", "net.minecraft.block.CactusBlock"})
+@Mixin(value = {SugarCaneBlock.class, CactusBlock.class})
 public abstract class TallboyVegetationMixin {
 	@ModifyConstant(method = "randomTick", constant = @Constant(intValue = 3))
 	private int maxint(int x) {
